874939a0f5f7bb74a130388ef5faf774d648f498
YoungMeatBoy <d2ms2nk@gmail.com>
2019-12-11 12:07:00 +0300

    upd setyp.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='headgen',
-	version='2.0.0',
+	version='2.0.1',
 	packages=find_packages(),
 	author='Miasnenko Dmitry',
 	author_email='d2ms2nk@gmail.com',
