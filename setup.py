44790aad4f3f5b62c220a368250820301d4b5132
YoungMeatBoy <d2ms2nk@gmail.com>
2019-12-23 10:36:33 +0300

    prepush commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='headgen',
-	version='2.3.1',
+	version='2.3.2',
 	packages=find_packages(),
 	author='Miasnenko Dmitry',
 	author_email='d2ms2nk@gmail.com',
